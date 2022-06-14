89c6e24a062f6e40b1114bc8a53657875c7a2dd7
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-14 20:48:05 +0900

    useState()で定義した値を使ってラジオボタンを切り替え

--- lib/features/auth/pages/auth_page.dart
+++ lib/features/auth/pages/auth_page.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../constants/global_variables.dart';
+
+enum Auth {
+  signIn,
+  signUp,
+}
 
 class AuthPage extends HookConsumerWidget {
   const AuthPage({super.key});
@@ -8,6 +16,49 @@ class AuthPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold();
+    final auth = useState<Auth>(Auth.signUp);
+    return Scaffold(
+      backgroundColor: greyBackgroundCOlor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              const Text(
+                'Welcome',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              ListTile(
+                title: const Text(
+                  'Create Account',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                leading: Radio(
+                  activeColor: secondaryColor,
+                  value: Auth.signUp,
+                  groupValue: auth.value,
+                  onChanged: (Auth? value) => auth.value = value!,
+                ),
+              ),
+              ListTile(
+                title: const Text(
+                  'Sing-In',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                leading: Radio(
+                  activeColor: secondaryColor,
+                  value: Auth.signIn,
+                  groupValue: auth.value,
+                  onChanged: (Auth? value) => auth.value = value!,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
