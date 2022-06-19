ecd99ad9252fe8fa1c9c5df67db7b434f336483f
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-19 18:53:12 +0900

    🔨refactor: AuthPage UI TestでRadioボタンをタップする順序を入れ替え カバレッジを上げるため

--- test/features/auth/pages/auth_page_test.dart
+++ test/features/auth/pages/auth_page_test.dart
@@ -16,15 +16,17 @@ void main() {
       // ジェネリクスでvalueプロパティに代入している値の型を指定しないとRadioは見つからない
       final radioFinder = find.byType(Radio<Auth>);
       expect(radioFinder, findsNWidgets(2));
-      // Keyを指定しているとWidgetを見つけやすい
-      await tester.tap(find.byKey(const Key('auth_page_radio_button_1')));
-      // Sign Upボタンをタップ
-      await tester.tap(find.byType(CustomButton).first);
       // 2つ目のRadioボタンをタップ
+      // Keyを指定しているとWidgetを見つけやすい
       await tester.tap(find.byKey(const Key('auth_page_radio_button_2')));
       // pump()でページを更新しないとSign Inボタンは見つからない
       await tester.pump();
       await tester.tap(find.text('Sign In'));
+
+      await tester.tap(find.byKey(const Key('auth_page_radio_button_1')));
+      await tester.pump();
+      // Sign Upボタンをタップ
+      await tester.tap(find.byType(CustomButton).first);
     },
   );
 }
