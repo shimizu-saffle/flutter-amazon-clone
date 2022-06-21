5144cf6bf94dd1a571fd14a68c13516d44cc7beb
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-21 21:55:36 +0900

    ✨feat: connect to mongodb

---
+++ server/index.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const authRouter = require("./routes/auth");
+
+// initialize
+const port = 3000;
+const app = express();
+const dbUri =
+  "mongodb+srv://kenta:*G46jmAkUpPg2C*@cluster0.to4jhyj.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(authRouter);
+
+// connect to mongodb
+mongoose
+  .connect()
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
