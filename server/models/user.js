3ef0a0aaadec55246529cadd95848ca8ba6cf710
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-22 21:20:40 +0900

    🐞fix: インデントの間違いを修正

--- server/models/user.js
+++ server/models/user.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   name: {
-    reqired: true,
-    trim: true,
     type: String,
+    required: true,
+    trim: true,
   },
   email: {
-    reqired: true,
-    trim: true,
     type: String,
+    required: true,
+    trim: true,
     validate: {
       validator: (value) => {
         // 正規表現は String型 で渡さない。
@@ -19,25 +19,25 @@ const userSchema = mongoose.Schema({
       },
       message: "Please enter a vailid email address",
     },
-    password: {
-      required: true,
-      type: String,
-      validate: {
-        validator: (value) => {
-          return value.length > 6;
-        },
-        message: "Please enter a longer password",
+  },
+  password: {
+    required: true,
+    type: String,
+    validate: {
+      validator: (value) => {
+        return value.length > 6;
       },
+      message: "Please enter a longer password",
     },
-    // 必須フィールドでは無いので、default値を指定している。
-    address: {
-      type: String,
-      default: "",
-    },
-    type: {
-      type: String,
-      default: "user",
-    },
+  },
+  // 必須フィールドでは無いので、default値を指定している。
+  address: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "user",
   },
   // cart
 });
