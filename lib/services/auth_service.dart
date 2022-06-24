bb59a0bb9b7d93daa93fdfe1ad1a028df0627f4f
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-24 09:09:07 +0900

    define: AuthService

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,15 @@
+class AuthService {
+  // sign up user
+  Future<void> signUpUser({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      // TODO(shimizu-saffle): implement signUpUser
+    } catch (e) {
+      print(e.toString());
+      rethrow;
+    }
+  }
+}
