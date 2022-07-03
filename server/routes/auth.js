f370ca6f29492588f4e466e263d0bc7fcc22589c
shimizu-saffle <shimizu.saffle@gmail.com>
2022-07-03 18:04:44 +0900

    ✨feat: create Sign Up Route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,9 @@ const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 const authRouter = express.Router();
+const jwt = require("jsonwebtoken");
 
+// Sign Up Route
 authRouter.post("/api/signup", async (req, res) => {
   try {
     // クライアントからデータを取得
@@ -34,6 +36,31 @@ authRouter.post("/api/signup", async (req, res) => {
     res.json(user);
   } catch (e) {
     res.status(500).json({ error: e.message });
+  }
+});
+
+// Sign In Route
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User with this email does not exist!" });
+    }
+
+    const isMatch = bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect password! Please try again!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+    res.json({ token, ...user._doc });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
